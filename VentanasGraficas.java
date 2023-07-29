e08679dcea156c60651ce0ac946f59987477ff41
Samanta Bautista <108688127+SambauR@users.noreply.github.com>
2023-07-29 14:06:53 -0600

    Actividades para ventanas gráficas usando Java

---
+++ VentanasGraficas.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.ventanasgraficas;
+
+public class VentanasGraficas {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanUno v1 = new VentanUno();
+        v1.setVisible(true);
+    }
+    
+}
